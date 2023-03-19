be19f883c2ba482f920fb16a63daac2ecabdcd7c
Toni Guli <toniguli16@gmail.com>
2023-03-19 11:16:38 +0100

    implemented find_closest_note(); changed Note enum; small test

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,82 @@
-pub enum Notes {
-    A(bool),
-    B,
-    C(bool),
-    D(bool),
-    E,
-    F(bool),
-    G(bool),
+const BASE_FREQ: f32 = 440.0;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Note {
+    A = 0,
+    AS = 1,
+    B = 2,
+    C = 3,
+    CS = 4,
+    D = 5,
+    DS = 6,
+    E = 7,
+    F = 8,
+    FS = 9,
+    G = 10,
+    GS = 11,
 }
 
-impl ToString for Notes {
+impl ToString for Note {
     fn to_string(&self) -> String {
         match self {
-            &Notes::A(false) => "A",
-            &Notes::A(true) => "A#",
-            &Notes::B => "B",
-            &Notes::C(false) => "C",
-            &Notes::C(true) => "C#",
-            &Notes::D(false) => "D",
-            &Notes::D(true) => "D#",
-            &Notes::E => "E",
-            &Notes::F(false) => "F",
-            &Notes::F(true) => "F#",
-            &Notes::G(false) => "G",
-            &Notes::G(true) => "G#",
+            &Note::A => "A",
+            &Note::AS => "A#",
+            &Note::B => "B",
+            &Note::C => "C",
+            &Note::CS => "C#",
+            &Note::D => "D",
+            &Note::DS => "D#",
+            &Note::E => "E",
+            &Note::F => "F",
+            &Note::FS => "F#",
+            &Note::G => "G",
+            &Note::GS => "G#",
         }
         .to_string()
+    }
+}
+
+impl TryFrom<u8> for Note {
+    type Error = ();
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        use Note::*;
+        match value {
+            0 => Ok(A),
+            1 => Ok(AS),
+            2 => Ok(B),
+            3 => Ok(C),
+            4 => Ok(CS),
+            5 => Ok(D),
+            6 => Ok(DS),
+            7 => Ok(E),
+            8 => Ok(F),
+            9 => Ok(FS),
+            10 => Ok(G),
+            11 => Ok(GS),
+            _ => Err(()),
+        }
+    }
+}
+
+pub fn find_closest_note(pitch: f32) -> (Note, f32) {
+    let note_index = ((pitch / BASE_FREQ).log2() * 12.0).round().rem_euclid(12.0) as u8;
+    let closest_note: Note = note_index.try_into().unwrap();
+    let closest_pitch = (BASE_FREQ * 2.0).powf(note_index as f32 / 12.0);
+
+    (closest_note, closest_pitch)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn find_closest_note() {
+        use super::*;
+
+        assert_eq!(find_closest_note(82.41).0, Note::E);
+        assert_eq!(find_closest_note(110.0).0, Note::A);
+        assert_eq!(find_closest_note(146.83).0, Note::D);
+        assert_eq!(find_closest_note(196.0).0, Note::G);
+        assert_eq!(find_closest_note(246.94).0, Note::B);
+        assert_eq!(find_closest_note(329.63).0, Note::E);
     }
 }
